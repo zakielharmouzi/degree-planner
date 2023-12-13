186828ab9fc4c9dba1291e501bfa0c54fc019db1
selmaa0 <selmamahidat@gmail.com>
2023-12-13 08:45:21 +0100

    hh

---
+++ Supabase_login_template/src/pages/GPACalculator.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import { useAuth } from '../../components/Authcontext';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+function GPACalculator(){
+    return(
+<div>awdi</div>
+    );
+}
--- Supabase_login_template/src/pages/fileupload.jsx
+++ Supabase_login_template/src/pages/fileupload.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../components/Authcontext';
 import supabase from '../../utils/Supabase'; 
+import { HashLoader } from "react-spinners";
 
 const FileUpload = () => {
   const { user } = useAuth();
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
   const types = ['application/pdf'];
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // 
+    }, 4000);
+
+    return () => clearTimeout(timer); 
+  }, []); 
   const pdfHandler = async (e) => {
     e.preventDefault();
     let selected = file;
@@ -23,6 +32,7 @@ const FileUpload = () => {
         console.log(data, error);
       } catch (error) {
         console.error('Error uploading file: ', error.message);
+        setError('Error uploading file. Please try again.');
       }
     } else {
       setFile(null);
@@ -43,14 +53,28 @@ const FileUpload = () => {
 
   return (
     <div>
-      <form onSubmit={pdfHandler}>
-        <input type="file" accept="application/pdf" onChange={handleFileChange} />
-        <div className="output">
-          {error && <div className="error">{error}</div>}
-          {file && <div>{file.name}</div>}
+      {loading && (
+        <div className="loader-container flex items-center justify-center h-screen">
+          <HashLoader color="#166432" loading={true} size={100} />
         </div>
-        <button type="submit">Upload</button>
-      </form>
+      )}
+      {!loading && (
+        <div className="mt-10">
+        <div className="font-Libre text-2xl">
+          <h1>Hello! Before accessing the website, please upload your unofficial transcript. :)</h1>
+          <h1 className="text-xl text-[#445858]">This is to check which course you have already taken.</h1>
+        </div>
+
+        <form onSubmit={pdfHandler}>
+          <input type="file" className="mt-5 font-Montserrat" accept="application/pdf" onChange={handleFileChange} />
+          <div className="mt-5 font-Montserrat output">
+            {error && <div className="error">{error}</div>}
+            {file && <div>{file.name}</div>}
+          </div>
+          <button type="submit" className="px-5 font-Montserrat mt-6 block bg-[#445858] text-white items-center whitespace-nowrap  py-[0.25rem] text-center text-base font-normal leading-[1.6] border border-[#9ca3af] outline-none p-3 h-10 ">Upload!</button>
+        </form>
+        </div>
+      )}
     </div>
   );
 };
