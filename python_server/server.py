136bc73d0d77664694c2da5c4dbf60db57236130
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2023-12-13 16:13:42 +0100

    made the course id and user_id matching

--- python_server/server.py
+++ python_server/server.py
@@ -35,13 +35,44 @@ def download_random_pdf():
     pattern = r'\b[A-Z]{3}\d+\b'
     pdfFileObj = open(destination, 'rb')
     pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    file = open("output.txt", "w")
     for page in pdfReader.pages:
         print(page.extract_text())
         text = page.extract_text()
         matches = re.findall(pattern, text)
+        courses = []
         for match in matches:
-            print(match)
+            match = match[:3] + " " + match[3:]
+            courses.append(match)
+            # print(match)
+        for course in courses:
+            response = supabase.table(
+                'courses').select("course_id").eq('course_code', course).execute()
+            # write the response to the file
+            file.write(str(response))
+            file.write("\n")
+    file.close()
+    # read the file and get the course ids:
+    file = open("output.txt", "r")
+    course_ids = []
+    pattern = r'\d+'
+    for line in file:
+        matches = re.findall(pattern, line)
+        for match in matches:
+            course_ids.append(match)
+    # remove duplicates
+    course_ids = list(dict.fromkeys(course_ids))
+    print(course_ids)
+    # insert the course ids into the database
+    for course_id in course_ids:
+        supabase.table('user_courses').insert(
+            {'id': user_id, 'course_id': course_id}).execute()
+    file.close()
     pdfFileObj.close()
+    # delete the file from the local filesystem
+    import os
+    os.remove(destination)
+    os.remove("output.txt")
     return "Hello World!"
 
 
