1b67b5df6d00673f9ac576c2acd1ceba0db060aa
zakaria elharmouzi <102734526+zakielharmouzi@users.noreply.github.com>
2024-07-28 00:37:25 +0100

    Update Supabase.jsx

--- Supabase_login_template/utils/Supabase.jsx
+++ Supabase_login_template/utils/Supabase.jsx
@@ -2,9 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 
 
-const SupabaseUrl = "https://jgikuxzyfsenajscnvau.supabase.co"
-const SupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnaWt1eHp5ZnNlbmFqc2NudmF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2NjY3OTAsImV4cCI6MjAxMTI0Mjc5MH0.ljjM1THnw1MbaxvU0OZq_Fhx-cVwn-X8MoV8tXiyAxU"
-
+const SupabaseUrl = KEY
+const SupabaseKey = KEY
 const supabase = createClient(SupabaseUrl, SupabaseKey);
 
 export default supabase;
